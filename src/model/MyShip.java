6177fecff345033762207f5c5cd83a4def1c8eea
anderson rueda guevara <you@example.com>
2020-04-08 15:07:48 -0500

    se agrogo un nuevo metodo para el total de la carg

--- src/model/MyShip.java
+++ src/model/MyShip.java
@@ -27,6 +27,48 @@ public class MyShip {
         return nameCaptain;
     }
 	
+	// calcula el costo de una nueva carga incluyemdo posibles descuentos, si el
+    // cliente es de tipo "Normal" no tiene descuento
+    public double costOfLoad(Load newLoad) {
+
+        double total = 0;
+
+        double weightTotal = 0;
+
+        if (newLoad.getTypeLoad().equals(Load.TYPELOAD[0])) {
+
+            weightTotal = newLoad.getWeightByLoad() * newLoad.getNumBoxes();
+            total = weightTotal * 390000;
+
+        } else if (newLoad.getTypeLoad().equals(Load.TYPELOAD[1])) {
+
+            weightTotal = newLoad.getWeightByLoad() * newLoad.getNumBoxes();
+            total = weightTotal * 250000;
+
+        } else {
+
+            weightTotal = newLoad.getWeightByLoad() * newLoad.getNumBoxes();
+            total = weightTotal * 80000;
+
+        }
+
+        if (newLoad.getOwner().getTypeClient().equals(Company.TYPESCLIENT[1])
+                && newLoad.getTypeLoad().equals(Load.TYPELOAD[1])) {
+            total -= total * Company.DISCOUNT[0];
+        }
+
+        if (newLoad.getOwner().getTypeClient().equals(Company.TYPESCLIENT[2])
+                && (newLoad.getTypeLoad().equals(Load.TYPELOAD[1]) || newLoad.getTypeLoad().equals(Load.TYPELOAD[2]))) {
+            total -= total * Company.DISCOUNT[1];
+        }
+
+        if (newLoad.getOwner().getTypeClient().equals(Company.TYPESCLIENT[3])) {
+            total -= total * Company.DISCOUNT[2];
+        }
+
+        return total;
+    }
+	
 	public Company[] getClients() {
         return clients;
     }
